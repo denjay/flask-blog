317570817902bfe52bb41f00b9dc6f25ee476a6c
denjay <295077487@qq.com>
2018-01-05 09:32:14 +0800

    完成第二部分

---
+++ app/api_1_0/posts.py
@@ -0,0 +1,30 @@
+from . import api
+from ..models import Post, Permission
+from .authentication import auth
+from flask import jsonify, request, url_for
+from ..decorators import permission_required
+from .. import db
+
+
+@api.route('/posts/')
+@auth.login_required
+def get_posts():
+    posts = Post.query.all()
+    return jsonify({'posts': [post.to_json() for post in posts]})
+
+
+@api.route('/posts/<int:id>')
+@auth.login_required
+def get_post(id):
+    post = Post.query.get_or_404(id)
+    return jsonify(post.to_json())
+
+
+@api.route('/posts/', methods=['POST'])
+@permission_required(Permission.WRITE_ARTICLES)
+def new_post():
+    post = Post.from_json(request.json)
+    post.author = g.current_user
+    db.session.add(post)
+    db.session.commit()
+    return jsonify(post.to_json()), 201, {'Location': url_for('api.get_post', id=post.id, _external=True)}
